561b5af2f9aa61a035020984ac79fcc0b5cbd7a6
GuiEnrik <gh.souza@hotmail.com>
2016-01-14 01:49:45 -0200

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -8,3 +8,29 @@ function saveDate(){
   var varDate = document.getElementById("inputDate").value;
   localStorage['stDate'] = varDate;
 }
+function fase(){
+		var dataHoje = new Date();
+		var dd = dataHoje.getDate();
+		var mm = dataHoje.getMonth()+1;
+		var yyyy = dataHoje.getFullYear();
+
+		var dataMenstruacao = document.getElementById("ultimaMenstruacao").value;
+		var dataAtual = dd+"/"+mm+"/"+yyyy;
+	
+	        var data1 = moment(dataMenstruacao, "YYYY-MM-DD");
+	        var data2 = moment(dataAtual, "DD/MM/YYYY");
+	
+	        var diferenca = data2.diff(data1, 'days');
+		alert(diferenca);
+		if(diferenca <= 5){
+		        alert("Menstrual");
+        	}else if(diferenca <= 13){
+		        alert("Folicular");
+        	}else if(diferenca <= 17){
+		        alert("Ovulatória");
+        	}else if(diferenca <= 23){
+		        alert("Lútea");
+        	}else{
+		        alert("TPM!!");
+		}
+	}
